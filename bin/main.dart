a77aea9499ca0b017ef45cd5743f858f2440fc82
showravdas11 <showravdas11@gmail.com>
2022-11-12 19:28:42 +0600

    list map and etc

---
+++ bin/main.dart
@@ -0,0 +1,89 @@
+void main() {
+  var map = new Map();
+  map.addAll({"name": "showrav", "Roll": 485298, "Dept": "CMT-A"});
+  // map.clear();
+  map.remove('Dept');
+  print(map);
+}
+
+// type non sefty
+// var name = 'showrav Das'
+
+// type sefty
+// String name = "showrav";
+
+  // var age = 5;
+  // if (age > 20) {
+  //   print("adult");
+  // } else if (age < 20) {
+  //   print("child");
+  // } else {
+  //   print("invalid age");
+  // }
+
+  //fixed length list
+  //   var fixedLengthList = List.filled(5, );
+  // fixedLengthList[0] = 87;
+  // fixedLengthList[1] = 87;
+  // fixedLengthList[2] = 87;
+  // fixedLengthList[3] = 87;
+  // fixedLengthList[4] = 87;
+
+  // print(fixedLengthList);
+
+  //growable list
+  //   var growableList = [];
+  // growableList.add(499);
+  // growableList.add(4599);
+  // growableList.add(4996);
+  // growableList.add(4996);
+  // growableList.add(4996);
+  // growableList.add(4996);
+  // growableList.add(4996);
+  // growableList.add(6);
+
+  // print(growableList);
+
+  //AddALL Method 
+  //   var growableList = [];
+  // growableList.addAll(["ARG", "BAN", "BRAZIL", "COL"]);
+
+  // insert Method 
+  //   growableList.insert(0, "Bisca el Barca");
+  // growableList.insert(1, "Bisca katalunia");
+  // print(growableList);
+  // insert method e index diye dite hobe 
+
+  // insertAll method 
+  //   var growableList = [];
+  // growableList.insertAll(0, ["A", "B", "C"]);
+  // print(growableList);
+
+  // Remove element in list ..............
+  //   var growableList = [];
+  // growableList.add(499);
+  // growableList.add(4599);
+  // growableList.add(4996);
+  // growableList.add(96);
+  // growableList.add(4996);
+  // growableList.add(10);
+  // growableList.add(4996);
+  // growableList.add(6);
+
+  // growableList.remove(96);......value dhore removed
+  // growableList.removeAt(5);..... index dhore removed
+  // print(growableList);
+
+  // map literels 
+  //   var mymap = {"name": "Showrav", "age": "19", "height": "5 feet 4 inch"};
+
+  // mymap['favTeam'] = "ARG";
+  // print(mymap);
+
+  // map constructor 
+  //   var mymap = new Map();
+  // mymap["name"] = "showrav";
+  // mymap["age"] = "19";
+  // mymap["team"] = "ARG";
+
+  // print(mymap);
